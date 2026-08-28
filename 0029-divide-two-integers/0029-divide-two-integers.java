b7946f33c8959325a6acc13a5aa00e93ac8998f0
Azhan1830 <azhanrizwan1830@gmail.com>
2026-08-28 18:41:51 +0530

    Time: 1 ms (95.65%), Space: 42.5 MB (68.7%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,11 +1,9 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        // Edge Case: Overflow handle karna
         if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE;
         }
 
-        // Final sign check karna
         boolean isNegative = (dividend < 0) != (divisor < 0);
 
         long n = Math.abs((long) dividend);
@@ -17,7 +15,6 @@ class Solution {
             long tempDivisor = d;
             long count = 1;
 
-            // Jab tak double kar sakte hain, double karte raho
             while (n >= (tempDivisor + tempDivisor)) {
                 tempDivisor += tempDivisor;
                 count += count;
