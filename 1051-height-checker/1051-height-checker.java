cab4ddfc9ca3b2eef9198de33779b77f9a837ddd
Azhan1830 <azhanrizwan1830@gmail.com>
2026-09-02 23:31:37 +0530

    Time: 0 ms (100%), Space: 42.8 MB (98.87%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,16 +1,21 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int[] arr = new int[heights.length]; // arr = expected height
-        for(int i=0; i<heights.length; i++){
-            arr[i] = heights[i];
-        }
+        int[] arr = new int[101]; // frequrncy array
 
-        Arrays.sort(arr);
+        for(int i: heights){
+            arr[i]++;
+        }
         int count = 0;
+        int curr_hei = 1;
+
         for(int i=0; i<heights.length; i++){
-            if(heights[i] != arr[i]){
+            while(arr[curr_hei] == 0){
+                curr_hei++;
+            }
+            if(heights[i] != curr_hei){
                 count++;
             }
+            arr[curr_hei]--;
         }
         return count;
     }
