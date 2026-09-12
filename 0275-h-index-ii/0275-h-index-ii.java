e443a3248a56c738a57aa49e8356d7d9d340a99f
Azhan Rizwan <azhanrizwan1830@gmail.com>
2026-09-12 23:01:25 +0530

    Time: 0 ms (100%), Space: 50.3 MB (27.32%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.java
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -2,16 +2,16 @@ class Solution {
     public int hIndex(int[] citations) {
         int n = citations.length;
         int left = 0;
-        int right = n-1;
+        int right = n - 1;
 
-        while(left<=right){
-            int mid = left + (right-left)/2;
-            int a = n-mid; // a = remaining papers
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            int a  = n - mid;
 
-            if (citations[mid]==a){
+            if(citations[mid] == a){
                 return a;
             }
-            else if (citations[mid] < a){
+            else if(citations[mid] < a){
                 left = mid + 1;
             }
             else{
